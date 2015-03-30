328ec3fa4b70c4c0467bc57c104da5c09dda2c63
Bruno Matias <b.100silvamatias@gmail.com>
2015-03-30 16:42:55 -0300

    Onde cheguei. Ex.vogais

---
+++ Exercicios/01_Vogais/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vogais_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //O programa deve ler uma mensagem de entrada padrão e dizer quantas vogais exitem na mensagem.
+
+
+            string palavra;
+         
+            Console.WriteLine("Digite uma mensagem de entrada padrão:");
+            palavra = string.Parse(Console.ReadLine());
+
+            
+
+             
+            
+
+
+        }
+
+    }
+}
