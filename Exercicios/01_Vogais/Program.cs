569f6cdd71b7eda0764a4e5927b450801fa537d6
Bruno Matias <b.100silvamatias@gmail.com>
2015-03-30 21:48:48 -0300

    Refazendo exercicio de vogais

--- Exercicios/01_Vogais/Program.cs
+++ Exercicios/01_Vogais/Program.cs
@@ -14,11 +14,20 @@ namespace Vogais_
             //O programa deve ler uma mensagem de entrada padrão e dizer quantas vogais exitem na mensagem.
 
 
-            string palavra;
+                char frase;
+	            int i, contador = 0;
          
             Console.WriteLine("Digite uma mensagem de entrada padrão:");
-            palavra = string.Parse(Console.ReadLine());
+            texto = int.Parse(Console.ReadLine());
 
+
+            for (int i = 0; i < texto.Length; i++)
+            {
+                if((frase[i]=='a')||(frase[i]=='i')||(frase[i]=='e')||(frase[i]=='o')||(frase[i]=='u'))
+		        {
+			        contador++;
+		        }
+            }
             
 
              
