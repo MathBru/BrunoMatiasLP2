f55640d135209e8444b22d1d9ca35e75b63d9b13
Bruno Silva <b.100silvamatias@gmail.com>
2015-06-23 16:40:41 -0300

    Exercicio à completar do dia 23/06/2015

---
+++ Exercicios/Aula_23062015/Aula_23062015/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula_23062015
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numFuncionarios = int.Parse(Console.ReadLine());
+
+           
+
+
+            for (int i = 0; i <= numFuncionarios; i++)
+            {
+                string nome = Console.ReadLine();
+                string cpf = Console.ReadLine();
+                DateTime DataNasc = Console.ReadLine();
+                double salario = Console.ReadLine();
+                DateTime dataAdmissao = Console.ReadLine();
+              
+
+             
+
+                if (dataAdmissao >= 2014)
+                {
+                    double aumento = 423.45;
+                    salario += aumento;
+
+                }
+
+                else if (dataAdmissao >= 2010 || dataAdmissao <= 2013 )
+                {
+                    salario += (salario / 100) * 9.84;
+                }
+
+                else if (dataAdmissao >= 2000 || dataAdmissao <= 2009)
+                {
+                    salario += (salario / 100) * 16.4;
+                }
+
+                else if (dataAdmissao <= 1999)
+                {
+                    salario += (salario / 100) * 22.6;
+                }
+
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
